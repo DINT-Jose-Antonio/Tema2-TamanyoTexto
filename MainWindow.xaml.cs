8966810fb3d7414dc65560932354cd7106855639
alumno <alumno@A302PC25>
2022-10-04 14:27:44 +0200

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_TamanyoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Boton1RadioButton.Tag = 36;
+            Boton2RadioButton.Tag = 48;
+            Boton3RadioButton.Tag = 72;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton boton = (RadioButton)sender;
+
+            textoTextBlock.FontSize = (int)boton.Tag;
+        }
+    }
+}
